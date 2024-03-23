db248602d689db68987b6d2346b8f8689f9ef283
matfuentesv <mat.fuentes@duocuc.cl>
2024-03-22 21:44:29 -0300

    subida de fuentes base api

---
+++ src/test/java/cl/company/ecommerce/apiecommerce/ApiECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.company.ecommerce.apiecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
